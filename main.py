ba37d619122a4fbd6976ed479ad784ee52ff1a80
Mohammad Tavakoli <tavakolimohammad969@gmail.com>
2022-09-01 02:33:42 +0430

    assign each packet to a flow, add function to print all flows

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import dpkt
 import socket
+import datetime
+
+
+def print_flows(dic):
+    counter = 1
+    for key, val in dic.items():
+        print('### flow number', counter)
+        print(key[0], ',', key[2], '-->', key[1], ',', key[3], ':', key[4], ':',
+              val[6], '; sent packets:', val[0], ', received packets:', val[1], ', sent bytes:', val[2],
+              ', received bytes:', val[3], ', timestamp: (', val[4], ',', val[5], ')')
+        counter += 1
 
 
 def find_flows(pcap):
+    dic = dict()
     for time, packet in pcap:
 
         ip = -1
-        ip_src = -1, ip_dst = -1
-        src_port = -1, dst_port = -1
+        ip_src = -1
+        ip_dst = -1
+        src_port = -1
+        dst_port = -1
         protocol = -1
 
         eth = dpkt.ethernet.Ethernet(packet)
@@ -26,6 +40,40 @@ def find_flows(pcap):
                 protocol = 'udp'
                 src_port = ip.data.sport
                 dst_port = ip.data.dport
+
+        if ip == -1 or \
+                ip_src == -1 or ip_dst == -1 or \
+                src_port == -1 or dst_port == -1 or \
+                protocol == -1:
+            continue
+
+        temp1_tuple = (ip_src, ip_dst, src_port, dst_port, protocol)
+        temp2_tuple = (ip_dst, ip_src, dst_port, src_port, protocol)
+
+        # sent packets, received packets, sent bytes, received bytes, first time, last time, protocol
+        test_list = [1,
+                     0,
+                     len(bytes(ip.data)),
+                     0,
+                     str(datetime.datetime.utcfromtimestamp(time)),
+                     str(datetime.datetime.utcfromtimestamp(time)),
+                     "Unknown"]
+
+        keys = dic.keys()
+
+        if temp1_tuple in keys:
+            dic[temp1_tuple][0] += 1
+            dic[temp1_tuple][2] += len(bytes(ip.data))
+            dic[temp1_tuple][5] = str(datetime.datetime.utcfromtimestamp(time))
+
+        elif temp2_tuple in keys:
+            dic[temp2_tuple][1] += 1
+            dic[temp2_tuple][3] += len(bytes(ip.data))
+            dic[temp2_tuple][5] = str(datetime.datetime.utcfromtimestamp(time))
+
+        else:
+            dic[temp1_tuple] = test_list
+    print_flows(dic)
 
 
 if __name__ == '__main__':
